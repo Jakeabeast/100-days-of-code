577a9e44a909f3794effe4ca773eb10cda19b344
Jake <103069596@student.swin.edu.au>
2024-03-07 19:21:30 +1100

    Completed Project01

---
+++ Day01/project.py
@@ -0,0 +1,15 @@
+'''
+DAY 1 PROJECT - Band Name Generator
+
+The goal of this project is to simply use prompts to get string inputs and concatonate them to create a band name based off the input.
+'''
+
+#=====================================================================
+
+print("Welcome to the Band Name Generator.")
+
+street = input("What's the name of the city you grew up in?\n")
+
+pet = input("What's your pet's name?\n")
+
+print("Your band name could be " + street + " " + pet)
