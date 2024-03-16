409bfe3c39e526483ee1b202ccd902dae0d96dc9
Jake <103069596@student.swin.edu.au>
2024-03-16 23:51:54 +1100

    Created calculator template

---
+++ Day10_Calculator/main.py
@@ -0,0 +1,46 @@
+from art import logo
+
+'''
+DAY 10 PROJECT - Calculator
+
+The goal of this project is to use create a calculator where the user can use an operation one step at a time. 
+For this project and hopefully onwards, I wont to try dedicating more commits to smaller chunks of code.
+This should help make it more clear what has been done and prepares me for future version control princples. 
+'''
+
+class Calculator():
+
+    def __init__():
+        pass
+
+    def __add():
+        pass
+
+    def __subtract():
+        pass
+
+    def __multiply():
+        pass
+
+    def __divide():
+        pass
+
+    def input():
+        pass
+
+    def update():
+        pass
+    
+    def output():
+        pass
+
+
+if __name__ == "__main__":
+    calc = Calculator()
+
+    while calc.calculating == True:
+        calc.input()
+        calc.update()
+        calc.output()
+
+    print("Goodbye")
